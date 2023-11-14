acbb76930006555e64ab35872ce1246d70c73bc2
ArgeniaP <35943150+ArgeniaP@users.noreply.github.com>
2023-11-14 15:08:45 +0700

    SESI 6: Nge-Trick Nested Data

--- src/libs/apiLibs.js
+++ src/libs/apiLibs.js
@@ -3,3 +3,20 @@ export const getAnimeResponse = async (resource, query) => {
     const anime = await response.json()
     return anime
 }
+
+export const getNestedAnimeResponse = async (resource, property) => {
+    const response = await getAnimeResponse(resource)
+
+    return response.data.flatMap(item => item[property])
+}
+
+export const reproduce = (data, gap) => {
+    const first = ~~(Math.random() * (data.length - gap) + 1)
+    const last = first + gap
+
+    const response = {
+        data: data.slice(first, last)
+    }
+
+    return response
+}
