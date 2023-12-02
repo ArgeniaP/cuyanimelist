8c3eba14f29ed40aefb276324a171bceeffc0082
ArgeniaP <35943150+ArgeniaP@users.noreply.github.com>
2023-12-02 15:27:08 +0700

    SESI 9: Validasi User Dashboard

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,13 +1,28 @@
 import { userAuthSession } from "@/libs/authLibs"
 import Image from "next/image"
+import Link from "next/link"
 
 const Page = async () => {
   const user = await userAuthSession()
   return (
-    <div className="text-color-primary">
-      <h3>DASHBORD</h3>
-      <p>Welcome, {user.name}</p>
+    <div className="mt-8 text-color-primary flex flex-col justify-center items-center">
+      <h5 className="text-2xl font-bold mb-2">Welcome, {user.name}</h5>
       <Image src={user.image} alt="..." width={250} height={250} />
+
+      <div className="flex flex-wrap gap-4 py-8">
+        <Link
+          href={"/users/dashboard/collection"}
+          className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl"
+        >
+          My Collections
+        </Link>
+        <Link
+          href={"/users/dashboard/comment"}
+          className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl"
+        >
+          My Comments
+        </Link>
+      </div>
     </div>
   )
 }
